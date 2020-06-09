66ac9852b3865ba6ea45b4cb09c853b6b5cd504e
Tomas Milar <tomasmilar4@gmail.com>
2020-06-09 01:25:08 -0300

    Validate express API params using '@sindresorhus/is'

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,10 +1,17 @@
 import { NextFunction, Request, Response } from "express"
 import { AgileEngineClient } from "../service/image-client"
+import is from "@sindresorhus/is"
+import HttpException from "../exception/http-exception"
 
 const agileEngine = new AgileEngineClient()
 
-export async function getImages(req: Request, res: Response, next: NextFunction) {
+export async function getImages(req: Request, res: Response, next: NextFunction): Promise<void> {
   const { page } = req.body
+
+  if (!is.number(page) || (is.number(page) && page < 0)) {
+    next(new HttpException(400, "Unexpected param 'page'"))
+    return
+  }
 
   try {
     const images = await agileEngine.getImages(page)
